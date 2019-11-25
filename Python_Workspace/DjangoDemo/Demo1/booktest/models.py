c4ad58196d30a27fd2d8c3ad4a03070db2223dea
lxconfig <525868229@qq.com>
2019-11-25 21:26:16 +0800

    Design Models Class

---
+++ Python_Workspace/DjangoDemo/Demo1/booktest/models.py
@@ -0,0 +1,25 @@
+'''
+@Author: lixuan
+@Date: 2019-11-25 20:02:34
+@LastEditTime: 2019-11-25 21:10:25
+@Description: design modelClass
+'''
+from django.db import models
+
+# Create your models here.
+
+class BookInfo(models.Model):
+    """图书模型类"""
+    # CharField表示字段是字符串，长度为20
+    book_title = models.CharField(max_length=20)
+    # DateField表示字段是日期类型
+    book_pub_date = models.DateField()
+
+class HeroInfo(models.Model):
+    """人物类"""
+    name = models.CharField(max_length=20)
+    gender = models.BooleanField(default=False)
+    comment =  models.CharField(max_length=128)
+    # 如果一个类(表)和其他类(表)存在一对多的关系，则需要在"多"中添加外键
+    # hbook代表一个关系属性,在表中是：关系属性名_id
+    hbook = models.ForeignKey("BookInfo")
