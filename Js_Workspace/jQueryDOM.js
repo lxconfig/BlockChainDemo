c9ced3c4593ddd7a77f218063b5ab25783986c9e
lxconfig <525868229@qq.com>
2019-11-26 21:02:04 +0800

    jQueryDOM

---
+++ Js_Workspace/jQueryDOM.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: lixuan
+ * @Date: 2019-11-26 10:48:46
+ * @LastEditTime: 2019-11-26 10:59:15
+ * @Description: 
+ */
+var js = $("#test-ul .js").text();
+var book = $("[name=book]").html();
