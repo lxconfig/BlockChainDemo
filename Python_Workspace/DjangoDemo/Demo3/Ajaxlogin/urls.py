c29c677cec637b4aea5791e0db106ff482b879e7
lxconfig <525868229@qq.com>
2019-12-03 20:43:47 +0800

    setcookie and getcookie

--- Python_Workspace/DjangoDemo/Demo3/Ajaxlogin/urls.py
+++ Python_Workspace/DjangoDemo/Demo3/Ajaxlogin/urls.py
@@ -3,6 +3,10 @@ from Ajaxlogin import views
 
 urlpatterns = [
     url(r'^login$', views.login),
+    url(r'^common_login$', views.common_login),
+    url(r'^common_login_check$', views.common_login_check),
     url(r'^login_check$', views.login_check),
     url(r'^index$', views.index),
+    url(r'^set_cookie$', views.set_cookie),
+    url(r'^get_cookie$', views.get_cookie),
 ]
--- Python_Workspace/DjangoDemo/Demo3/Ajaxlogin/views.py
+++ Python_Workspace/DjangoDemo/Demo3/Ajaxlogin/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
+from datetime import datetime, timedelta
 
 # Create your views here.
 
 def login(request):
     return render(request, 'Ajaxlogin/login.html')
+
+def common_login(request):
+    if 'username' in request.COOKIES:
+        username = request.COOKIES['username']
+    else:
+        username = ''
+    return render(request, 'Ajaxlogin/common_login.html', {'username': username})
+
+def common_login_check(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    remember = request.POST.get('remember')  # 点击后传过来的是on，否则是None
+    if username == 'lixuan' and password == '123':
+        response = redirect('/index')
+        if remember == 'on':
+            response.set_cookie('username', username, max_age=7*24*3600)
+        return response
+    else:
+        return redirect('/common_login')
 
 def login_check(request):
     username = request.POST.get('username')
@@ -20,3 +40,20 @@ def login_check(request):
 
 def index(request):
     return render(request, 'Ajaxlogin/index.html')
+
+def set_cookie(request):
+    # 必须是HttpResponse类的对象，或其子类的对象才能设置cookie
+    response = HttpResponse('设置cookie')
+    # 设置一个cookie信息，名字为num，值为1
+    response.set_cookie('num', 1)
+    # 设置过期时间,max_age表示多少秒后过期
+    # response.set_cookie('num', 1, max_age=14*24*3600)
+    # expires表示多少天后过期
+    # response.set_cookie('num', 1, expires=datetime.now()+timedelta(day=14))
+    # 返回cookie给浏览器
+    return response
+
+def get_cookie(request):
+    # 取cookie的值
+    num = request.COOKIES['num']
+    return HttpResponse(num)
