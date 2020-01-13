6974cec74c84526dcb4fb79c22d4b0dda8cdeb18
lxconfig <525868229@qq.com>
2020-01-13 17:22:16 +0800

    缓存

--- Python_Workspace/DjangoDemo/dailyFresh/apps/goods/admin.py
+++ Python_Workspace/DjangoDemo/dailyFresh/apps/goods/admin.py
@@ -1,11 +1,60 @@
 from django.contrib import admin
 from apps.goods.models import Goods, GoodsImage, GoodsSKU, GoodsType, IndexGoodsBanner, IndexPromotionBanner, IndexTypeGoodsBanner
+from django.core.cache import cache
 # Register your models here.
 
-admin.site.register(Goods)
-admin.site.register(GoodsImage)
-admin.site.register(GoodsSKU)
-admin.site.register(GoodsType)
-admin.site.register(IndexTypeGoodsBanner)
-admin.site.register(IndexPromotionBanner)
-admin.site.register(IndexGoodsBanner)
+
+class BaseModel(admin.ModelAdmin):
+    """商品种类模型管理器类"""
+    def save_model(self, request, obj, form, change):
+        """新增或更新表中的数据时调用"""
+        super().save_model(request, obj, form, change)
+
+        # 发出任务，让celery_worker重新生成静态首页
+        # 导入模块不要写到函数外，否则启动不了项目
+        from celery_tasks.tasks import generate_static_index_html
+        generate_static_index_html.delay()
+
+        # 清除首页的缓存数据
+        cache.delete('index_page_data')
+    
+    def delete_model(self, request, obj):
+        """删除表中的数据时调用"""
+        super().delete_model(request, obj)
+
+        # 发出任务，让celery_worker重新生成静态首页
+        from celery_tasks.tasks import generate_static_index_html
+        generate_static_index_html.delay()
+
+        # 清除首页的缓存数据
+        cache.delete('index_page_data')
+
+
+class GoodsAdmin(BaseModel):
+    pass
+
+class GoodsImageAdmin(BaseModel):
+    pass
+
+class GoodsTypeAdmin(BaseModel):
+    pass
+
+class GoodsSKUAdmin(BaseModel):
+    pass
+
+class IndexGoodsBannerAdmin(BaseModel):
+    pass
+
+class IndexPromotionBannerAdmin(BaseModel):
+    pass
+
+class IndexTypeGoodsBannerAdmin(BaseModel):
+    pass
+
+admin.site.register(Goods, GoodsAdmin)
+admin.site.register(GoodsImage, GoodsImageAdmin)
+admin.site.register(GoodsSKU, GoodsSKUAdmin)
+admin.site.register(GoodsType, GoodsTypeAdmin)
+admin.site.register(IndexTypeGoodsBanner, IndexTypeGoodsBannerAdmin)
+admin.site.register(IndexPromotionBanner, IndexPromotionBannerAdmin)
+admin.site.register(IndexGoodsBanner, IndexGoodsBannerAdmin)
--- Python_Workspace/DjangoDemo/dailyFresh/apps/goods/views.py
+++ Python_Workspace/DjangoDemo/dailyFresh/apps/goods/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import View
 from apps.goods.models import GoodsType,IndexGoodsBanner,IndexPromotionBanner,IndexTypeGoodsBanner
 from django_redis import get_redis_connection
+from django.core.cache import cache   # 设置django缓存
 
 # Create your views here.
 
@@ -9,26 +10,39 @@ from django_redis import get_redis_connection
 class IndexView(View):
     def get(self, request):
         """首页类视图"""
-        # 获取商品的种类信息
-        types = GoodsType.objects.all()
+        # 首先查看缓存中是否存在数据
+        context = cache.get('index_page_data')
+        if context is None:
+            """无缓存，则需要读取数据"""
+            print("设置缓存")
+            # 获取商品的种类信息
+            types = GoodsType.objects.all()
 
-        # 获取首页轮播商品的信息
-        # 按index进行升序排列
-        goods_banners = IndexGoodsBanner.objects.all().order_by('index')
+            # 获取首页轮播商品的信息
+            # 按index进行升序排列
+            goods_banners = IndexGoodsBanner.objects.all().order_by('index')
 
-        # 获取首页促销商品的信息
-        promotion_banners = IndexPromotionBanner.objects.all().order_by('index')
+            # 获取首页促销商品的信息
+            promotion_banners = IndexPromotionBanner.objects.all().order_by('index')
 
-        # 获取首页分类展示的商品信息(有图片及文字两种)
-        # type_goods_banners = IndexTypeGoodsBanner.objects.all()
-        for type in types:
-            # 根据type种类，获取首页分类商品的文字信息和图片信息
-            banners_title = IndexTypeGoodsBanner.objects.filter(type=type, display_type=0).order_by('index')
-            banners_image = IndexTypeGoodsBanner.objects.filter(type=type, display_type=1).order_by('index')
-        
-        # 动态的添加两种属性
-        types.banners_title = banners_title
-        types.banners_image = banners_image
+            # 获取首页分类展示的商品信息(有图片及文字两种)
+            # type_goods_banners = IndexTypeGoodsBanner.objects.all()
+            for type in types:
+                # 根据type种类，获取首页分类商品的文字信息和图片信息
+                banners_title = IndexTypeGoodsBanner.objects.filter(type=type, display_type=0).order_by('index')
+                banners_image = IndexTypeGoodsBanner.objects.filter(type=type, display_type=1).order_by('index')
+            
+            # 动态的添加两种属性
+            types.banners_title = banners_title
+            types.banners_image = banners_image
+
+            context = {
+                "types": types,
+                "goods_banners": goods_banners,
+                "promotion_banners": promotion_banners,
+            }
+            # 设置缓存(key, value, timeout)
+            cache.set('index_page_data', context, 3600)
 
         # 获取首页用户购物车商品数量
         user = request.user
@@ -42,11 +56,7 @@ class IndexView(View):
             cart_count = conn.hlen(cart_key)
 
         # 组织上下文
-        context = {
-            "types": types,
-            "goods_banners": goods_banners,
-            "promotion_banners": promotion_banners,
-            "cart_count": cart_count,
-        }
+        context.update(cart_count=cart_count)
+
         # 返回响应
         return render(request, 'index.html', context)
